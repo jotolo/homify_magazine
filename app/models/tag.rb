f66091170ef38c1f4a30c866b66085a619d409cc
Jorge T <jotolo7@gmail.com>
2017-10-14 15:43:18 -0400

    fixing relationship between articles and tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
   validates_presence_of :name
   has_many :sub_tags
-  has_and_belong_to_many :articles
+  has_and_belongs_to_many :articles
 end
--- db/migrate/20171014193817_create_articles_tags_join_table.rb
+++ db/migrate/20171014193817_create_articles_tags_join_table.rb
@@ -1,5 +1,5 @@
 class CreateArticlesTagsJoinTable < ActiveRecord::Migration[5.1]
   def change
-    create_join_table(:article, :tag)
+    create_join_table(:articles, :tags)
   end
 end
